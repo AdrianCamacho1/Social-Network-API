724a5c890b6ebf0d6fc3d6b7aac8b6a97471ce9f
AdrianCamacho1 <Camacho0600@gmail.com>
2023-12-03 00:01:39 -0500

    User updated

---
+++ controllers/userController.js
@@ -0,0 +1,41 @@
+const { User, Thought } = require("../models");
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({
+            path: "friends",
+            select: "-__v",
+        })
+        .select("-__v")
+        .sort({ _id: -1 })
+        .then((dbUserData)=> res.json(dbUserData))
+        .catch((err)=> {
+            console.log(err);
+            res.sendStatus(500);
+        });
+    },
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: "thoughts",
+            select: "-__v",
+        })
+        .populate({
+            path: "friends",
+            select: "-__v",
+        })
+        .select("-__v")
+        .then((dbUserData)=> {
+            if (!dbUserData) {
+                return res.status(404).json({ message: "error"});
+            }
+            res.json(dbUserData);
+        })
+        .catch((err)=> {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+    
+}
