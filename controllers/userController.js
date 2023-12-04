5b081dfb624e3edb97d38a26a4ed2aedf6b70cab
AdrianCamacho1 <Camacho0600@gmail.com>
2023-12-04 18:21:46 -0500

    updated crud method

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,5 +37,54 @@ const userController = {
             res.sendStatus(400);
         });
     },
-    
-}
+
+    createUser ({ body }, res) {
+        User.create(body)
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err)=> res.json(err))
+    },
+    updateUser({ params, body }, res ) {
+        User.findOneAndUpdate({ _id: params.id }, body, {
+            new: true,
+            runValidators: true,
+        })
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "error"});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch((err)=> res.json(err));
+    },
+    addFriend({ params }, res ){
+        User.findOneAndUpdate(
+            {_id: params.userId},
+            {$addToSet: {friends: params.friendId}},
+            { new: true, runValidators: true }
+        )
+        .then((dbUserData)=> {
+            if (!dbUserData) {
+                res.status(404).json ({ message: "error"});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch((err)=> res.json(err));
+    },
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId},
+            { $pull: { friends: params.friendId }},
+            { new: true }
+        )
+        .then ((dbUserData)=> {
+            if(!dbUserData) {
+                return res.status(404).json({ message: "error"});
+            }
+            res.json(dbUserData);
+        })
+        .catch((err)=> res.json(err));
+    },
+};
+module.exports = userController;
